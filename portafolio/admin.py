d6c38b9a47a715785f7fa1a38bcd622abc4500ad
julian <juliandr544@gmail.com>
2023-02-04 17:27:28 -0500

    rm blog, and add info from models

--- portafolio/admin.py
+++ portafolio/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Projects)
+admin.site.register(models.Skills)
+admin.site.register(models.Profiles)
--- portafolio/models.py
+++ portafolio/models.py
@@ -6,3 +6,18 @@ class Projects(models.Model):
     image= models.ImageField(upload_to="portafolio/images")
     url= models.URLField(blank=True)
 
+class Profiles(models.Model):
+    name = models.CharField(max_length=50) # example : Julian
+    title = models.CharField(max_length=50) # example : Django Developer 
+    description = models.CharField(max_length=500) # example : I am someone who loves python--
+    image= models.ImageField(upload_to="portafolio/images") # example : photo of you
+    linkedin = models.CharField(max_length=50) #link
+    github = models.CharField(max_length=50) #link
+
+class Skills(models.Model): 
+    name = models.CharField(max_length=50) # example : Docker
+    image= models.ImageField(upload_to="portafolio/images") # example : icon of python
+    
+
+
+
